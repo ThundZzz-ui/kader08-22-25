aedaba9c077a2a4b858a7ea0e7e68113b7d8b0c0
ThundZzz-ui <petirprambono@gmail.com>
2025-11-08 15:43:50 +0700

    greetings in c++

---
+++ greetings.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "nama : Petir Bani Purambono" << endl;
+cout << "hobi : Olahraga" << endl;
+cout << "alamat : Godean, Dusun Geplakan" << endl;
+
+}
